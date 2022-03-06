e2fdd606dcf78627f7c66caed2b2cfc9ba25fd6e
Jaimelyn Cruz <50213559+jaimelyncruz@users.noreply.github.com>
2022-03-05 19:29:43 -0800

    Add files via upload

--- Source/Camera.cpp
+++ Source/Camera.cpp
@@ -1,66 +1,25 @@
-#include "glm.0.9.7.1/build/native/include/glm/vec3.hpp"
-#include "Ray.h"
+#include <Math.h>
+#include "glm-0.9.7.1/glm/vec3.hpp"
+#include "glm-0.9.7.1/glm/glm.hpp"
+#include "glm-0.9.7.1/glm/gtc/matrix_transform.hpp"
 #include "Camera.h"
-#include "Image.h"
-#include "Readfile.h"
-#include "Scene.h"
 
-Camera::Camera() {
+Camera::Camera(vec3 lookFrom, vec3 lookAt, vec3 up, float fovy, Scene scene) {
+    w = glm::normalize(lookFrom-lookAt);
+    u = glm::normalize(glm::cross(up, w));
+    v = glm::cross(w, u);
+    fovy = glm::radians(fovy);
+    fovx = tan(fovy/2) * (scene.getSceneWidth()/scene.getSceneHeight());
+    fovx = 2 * atan(fovx);
+}
+
+Ray Camera::RayThruPixel(int pixelX, int pixY, Scene scene) {
+    //not sure about alpha and beta!!
+    int halfSceneWidth = scene.getSceneWidth()/2;
+    int halfSceneHeight = scene.getSceneHeight()/2;
     
-}
-
-Ray RayThruPixel(w,h){
-    glm::vec3 rayVec = glm::vec3(w, h, -1); //(0,0,-1) is what the camera eyenorm is set to.
-    Ray retRay = Ray(centerNorm, rayVec); //Ray whos origin is the camera and direction is the generated ray vector.
-    return retRay;
-}
-
-Image Raytrace(Scene scene){
-    int sw = scene.width; int sh = scene.height;
-    Image image = new Image(sw, sh); //Generate a new image of the correct size
-    for(int h=0; h<sh; h++){ //For every height pos
-        for(int w=0; w<sw; w++){ //For every width pos
-            Ray ray = RayThruPixel(w, h); //Generate a ray from the pixel at w,h
-            Intersection hit = Intersect(ray); //For this ray, return the intersection closest to the origin
-            image.pixels[w][h] = FindColor(hit); //Get a color given that intersection and store it in the image array
-        }
-    }
-    return image; //Return the image
-}
-
-Intersection Intersect(Ray ray){
-    Intersect retVal;
-    float dist;
-    for(/*list of objects in the scene*/;;){
-        switch(/*obj.type*/){
-            case sphere: 
-                Intersect newVal = sphereIntersect(ray, obj).distance;
-                newdist = retVal.dist;
-                if(newdist < dist){ //Only update the working object if the object is infront of everything else we've checked so far.
-                    dist = newdist;
-                    retVal = newVal;
-                } 
-                break;
-            case triangle:
-                Intersect newVal = triangleIntersect(ray, obj).distance;
-                newdist = retVal.dist;
-                if(newdist < dist){
-                    dist = newdist;
-                    retVal = newVal;
-                } 
-                break;
-            default:
-                return nullptr;
-        }
-    }
-    //Return the Intersect with the shortest distance
-}
-
-Intersection sphereIntersect(ray, sphere){ //We need sphere and triangle objects
-    //Sphere hit logic
-    //if hit then return correct Intersection else return intersection with dist set to infinity
-}
-Intersection triangleIntersect(ray, tri){
-    //Triangle hit logic
-    //if hit return correct Intersection else return intersection with dist set to infinity
+    float alpha = tan(fovx/2)*((pixY-halfSceneWidth)/halfSceneWidth);
+    float beta = tan(fovy/2)*((halfSceneHeight-pixelX)/halfSceneHeight);
+    vec3 direction = normalize(alpha*u + beta*u - w);
+    return Ray(lookFrom, direction);
 }
--- Source/Camera.h
+++ Source/Camera.h
@@ -1,15 +1,29 @@
-#include "glm.0.9.7.1/build/native/include/glm/vec3.hpp"
+#include "glm-0.9.7.1/glm/vec3.hpp"
+#include "Ray.h"
+#include "Scene.h"
+
+typedef glm::vec3 vec3 ; 
 
 class Camera {
     
     public:
         Camera();
+        Camera(vec3 lookFrom, vec3 lookAt, vec3 up, float fovy, Scene scene);
         //method to generate rays to each pixel 
-    
-    
-        glm::vec3 eyeNorm = { 0, 0, -1 };
-        glm::vec3 centerNorm = { 0, 0, 0 };
-        glm::vec3 upNorm = { 0, 1, 0 };
-        
-        int fovy = 45;
+        void setEye(vec3 eyeInput) {lookFrom = eyeInput;};
+        void setlookAt(vec3 lookAtInput) {lookAt = lookAtInput;};
+        void setUp(vec3 upInput) {up = upInput;};
+        void setfov(float fovInput) {fovy = fovInput;};
+        Ray RayThruPixel(int pixelX, int pixY, Scene scene);
+
+    private:
+        vec3 lookFrom;
+        vec3 lookAt;
+        vec3 up;
+        vec3 fov;
+        vec3 w;
+        vec3 u;
+        vec3 v;
+        float fovy = 45.0;
+        float fovx;
 };
