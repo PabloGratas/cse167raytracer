5f93cbdd76938f67113dd8169d2978ca36856157
PabloGratas <pegratas@gmail.com>
2022-03-14 00:03:15 -0700

    added lighting variables to objects
    currently no functionallity, eventually readfile should be able to take in any one of these values and apply it to any new objects created

--- Source/Sphere.h
+++ Source/Sphere.h
@@ -5,6 +5,13 @@ class Sphere{
     public: 
         point center; //Center of the sphere.
         float rad; //Radius of the sphere.
+        glm::vec3 ambient;
+        glm::vec3 directionala;
+        glm::vec3 directionalb;
+        glm::vec3 pointa;
+        glm::vec3 pointb;
+        glm::vec3 diffuse;
+        glm::vec3 specular;
         Sphere(point center, float rad);
         ~Sphere();
 };
--- Source/Triangle.h
+++ Source/Triangle.h
@@ -6,6 +6,13 @@ class Triangle{
         point a;
         point b;
         point c;
+        glm::vec3 ambient;
+        glm::vec3 directionala;
+        glm::vec3 directionalb;
+        glm::vec3 pointa;
+        glm::vec3 pointb;
+        glm::vec3 diffuse;
+        glm::vec3 specular;
         Triangle(point a, point b, point c);
         ~Triangle();
 };
