3ee6f0b71a9287655fbc653f6cd74624fbc7bbb8
PabloGratas <pegratas@gmail.com>
2022-03-14 20:19:21 -0700

    pushed

--- Source/main.cpp
+++ Source/main.cpp
@@ -44,7 +44,7 @@ int main ( int argc, char* argv[] ) {
     for(auto itr = cams.begin; itr < cams.end; itr++){
         bitmap itr->bitmapBuild();
         //FOR TEST we will need to 
-        if(FreeImage_Save(FIF_PNG, bitmap , "test.png", 0 )) {
+        if(FreeImage_Save(FIF_PNG, bitmap , "image1.png", 0 )) {
             cout << "Image successfully saved!" << endl ;
         }
     }
